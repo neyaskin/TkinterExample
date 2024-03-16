e7b4b702de08a2b0d723b2935aa68197be27a684
neyaskin <neiasckin@gmail.com>
2024-03-16 11:48:43 +0700

    Add Label

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ window.title('Welcome')
 # Изменяем размеры окна width x height
 window.geometry('400x300')
 
+label = Label(text='Hello World!', font=('Arial', 16))
+label.place(x=0, y=0)
+
 # Вызываем функцию, чтобы окно не закрывалось сразу после открытия
 window.mainloop()
